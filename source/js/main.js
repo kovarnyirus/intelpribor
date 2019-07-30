239668436034547146acfea247c77e1d95543a97
kovarnyirus <kovarnyi@gmail.com>
2019-07-30 16:48:40 +0300

    save

--- source/js/main.js
+++ source/js/main.js
@@ -1,1 +1,40 @@
 'use strict';
+
+
+$('.price__slider-list').slick({
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  arrows: false,
+  adaptiveHeight: true
+});
+
+$('.examples-slider-list').slick({
+  // dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  arrows: true,
+  adaptiveHeight: true,
+  nextArrow: $('.examples-slider__next'),
+  prevArrow: $('.examples-slider__prev')
+});
+
+$('.certificates__slider-list').slick({
+  // dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  adaptiveHeight: true,
+  arrows: true,
+  adaptiveHeight: true,
+  nextArrow: $('.certificates__next'),
+  prevArrow: $('.certificates__prev')
+});
+
+
+$('.answers__item').on('click', function (e) {
+  $(this).toggleClass('answers__item--open');
+
+})
