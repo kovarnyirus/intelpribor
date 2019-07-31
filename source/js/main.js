2e20de8e5a4ec49d15ad609c15b20b930c762840
kovarnyirus <kovarnyi@gmail.com>
2019-07-31 10:26:21 +0300

    mobile

--- source/js/main.js
+++ source/js/main.js
@@ -7,7 +7,29 @@ $('.price__slider-list').slick({
   speed: 300,
   slidesToShow: 1,
   arrows: false,
-  adaptiveHeight: true
+  adaptiveHeight: true,
+  nextArrow: $('.price__slider-next'),
+  prevArrow: $('.price__slider-prev'),
+  responsive: [
+    {
+      breakpoint: 2000,
+      settings: {
+        slidesToShow: 3,
+      }
+    },
+    {
+      breakpoint: 980,
+      settings: {
+        slidesToShow: 2,
+      }
+    },
+    {
+      breakpoint: 540,
+      settings: {
+        slidesToShow: 1,
+      }
+    }
+  ]
 });
 
 $('.examples-slider-list').slick({
@@ -18,7 +40,21 @@ $('.examples-slider-list').slick({
   arrows: true,
   adaptiveHeight: true,
   nextArrow: $('.examples-slider__next'),
-  prevArrow: $('.examples-slider__prev')
+  prevArrow: $('.examples-slider__prev'),
+  responsive: [
+    {
+      breakpoint: 2000,
+      settings: {
+        slidesToShow: 2,
+      }
+    },
+    {
+      breakpoint: 540,
+      settings: {
+        slidesToShow: 1,
+      }
+    }
+  ]
 });
 
 $('.certificates__slider-list').slick({
@@ -30,11 +66,30 @@ $('.certificates__slider-list').slick({
   arrows: true,
   adaptiveHeight: true,
   nextArrow: $('.certificates__next'),
-  prevArrow: $('.certificates__prev')
+  prevArrow: $('.certificates__prev'),
+  responsive: [
+    {
+      breakpoint: 2000,
+      settings: {
+        slidesToShow: 4,
+      }
+    },
+    {
+      breakpoint: 980,
+      settings: {
+        slidesToShow: 2,
+      }
+    },
+    {
+      breakpoint: 540,
+      settings: {
+        slidesToShow: 1,
+      }
+    }
+  ]
 });
 
 
 $('.answers__item').on('click', function (e) {
   $(this).toggleClass('answers__item--open');
-
-})
+});
